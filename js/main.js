9edb44e16bb9c2cb3d6af77bd73c767fd9088a67
arial-alisha <aria0023@algonquinlive.com>
2016-04-06 12:15:13 -0400

    changes

--- js/main.js
+++ js/main.js
@@ -10,6 +10,10 @@ $body.on('click', '.btn-move', function () {
   $('.diamond').toggleClass('diamond-move');
 });
 
-$body.on('click', 'btn-collapse-expand', function () {
+$body.on('click', '.btn-collapse-expand', function () {
   $('.panel').toggleClass('panel-collapse');
+});
+
+$body.on('click', '.btn-bounce', function () {
+  $('.circle').toggleClass('circle-bounce');
 });
